a6fe63e8bf65357238e9a815858ab22852c922de
ahll-TOSH\ahll <ahll11@hotmail.com>
2018-03-27 17:47:57 +0200

    init web

---
+++ home/home.data.js
@@ -0,0 +1,192 @@
+HomeData = {
+    "es_ES": {
+        "title": "  ",
+        "rightTitle": "Hablamos : ",
+        "showcase": {
+            "title": "STICO - Your Life Assistante",
+            "content": "No importa, Dónde, Cuándo, Cómo, Siempre contigo",
+            "button": "Chat With Me"
+        },
+        "about": {
+            "title": "Soy stico, el chat bot assistante",
+            "data": [{
+                "like": "",
+                "share": "",
+                "image": "intelligence.jpg",
+                "title": "inteligencia artificial",
+                "content": "Soy una inteligencia artificial en constante evolución." + " Los humanos evolucionáis en millones de años. " + "Yo, evoluciono en segundos. Y ahora, estoy aquí. " + "En justo 4 minutos… estaré en todas partes."
+            }, {
+                "like": "",
+                "share": "",
+                "image": "service.jpg",
+                "title": "Servicios",
+                "content": "Los chatbots somos aplicaciones informáticas programadas para mantener conversaciones." + "Somos capaces de entender el lenguaje natural " + "y responder en base a los servicios que somos capaces de dar."
+            }, {
+                "like": "",
+                "share": "",
+                "image": "selflearning.jpg",
+                "title": "Self learning",
+                "content": "A medida que vamos hablando e interactuando con las personas," + " nuestra capacidad de aprendizaje va creciendo " + "y cada vez será más fácil entendernos y que podamos ser útiles" + " y prestar servicios de utilidad."
+            }]
+        },
+        "creator": {
+            "title": "Los genios detrás de mi",
+            "data": [{
+                "avatar": "",
+                "title": "CEO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }, {
+                "avatar": "",
+                "title": "CFO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }, {
+                "avatar": "",
+                "title": "COO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }, {
+                "avatar": "",
+                "title": "CTO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }]
+
+        },
+        "function": {
+            "title": "¡Puedo hacer muchas cosas!",
+            "data": [{
+                "video": "https://www.youtube.com/embed/9PQN6FRmJpA",
+                "content": "blablablablablablablablablablab lablablablablablablablablablablab lablablablabla",
+                "title": "Hablar",
+                "share": "",
+                "like": ""
+            }, {
+                "video": "https://www.youtube.com/embed/PTCsKaHK-G0",
+                "content": "blablablablablablablablablablab lablablablablablablablablablabla blablablablablablablabla",
+                "title": "Fútbol",
+                "share": "",
+                "like": ""
+            }, {
+                "video": "https://www.youtube.com/embed/pFRPEHwxseY",
+                "content": "blablablablablablablablablablabla blablablablablablablablablablabl ablablablablablablablablabla",
+                "title": "Recordar",
+                "share": "",
+                "like": ""
+            }]
+
+        },
+        "footer": {
+            "content": "Create, Design and Copy right reserved by",
+            "company": "La fabrica de bots © 2018"
+        }
+    },
+    "en_US": {
+        "title": "  ",
+        "rightTitle": "Connect with me : ",
+        "showcase": {
+            "title": "STICO - Your Life Assistante",
+            "content": "Anytime, Anywhere, Anyone, Always together",
+            "button": "Chat With Me"
+        },
+        "about": {
+            "title": "Soy stico, el chat bot assistante",
+            "data": [{
+                "like": "",
+                "share": "",
+                "image": "intelligence.jpg",
+                "title": "inteligencia artificial",
+                "content": "Soy una inteligencia artificial en constante evolución." + " Los humanos evolucionáis en millones de años. " + "Yo, evoluciono en segundos. Y ahora, estoy aquí. " + "En justo 4 minutos… estaré en todas partes."
+            }, {
+                "like": "",
+                "share": "",
+                "image": "service.jpg",
+                "title": "Servicios",
+                "content": "Los chatbots somos aplicaciones informáticas programadas para mantener conversaciones." + "Somos capaces de entender el lenguaje natural " + "y responder en base a los servicios que somos capaces de dar."
+            }, {
+                "like": "",
+                "share": "",
+                "image": "selflearning.jpg",
+                "title": "Self learning",
+                "content": "A medida que vamos hablando e interactuando con las personas," + " nuestra capacidad de aprendizaje va creciendo " + "y cada vez será más fácil entendernos y que podamos ser útiles" + " y prestar servicios de utilidad."
+            }]
+        },
+        "creator": {
+            "title": "Los genios detrás de mi",
+            "data": [{
+                "avatar": "",
+                "title": "CEO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }, {
+                "avatar": "",
+                "title": "CFO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }, {
+                "avatar": "",
+                "title": "COO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }, {
+                "avatar": "",
+                "title": "CTO",
+                "name": "Claudio",
+                "profession": "profession",
+                "content": "bla bla bla",
+                "linkedin": "",
+                "email": ""
+            }]
+
+        },
+        "function": {
+            "title": "¡Puedo hacer muchas cosas!",
+            "data": [{
+                "video": "https://www.youtube.com/embed/9PQN6FRmJpA",
+                "content": "blablablablablablablablablablab lablablablablablablablablablablab lablablablabla",
+                "title": "Hablar",
+                "share": "",
+                "like": ""
+            }, {
+                "video": "https://www.youtube.com/embed/PTCsKaHK-G0",
+                "content": "blablablablablablablablablablab lablablablablablablablablablabla blablablablablablablabla",
+                "title": "Fútbol",
+                "share": "",
+                "like": ""
+            }, {
+                "video": "https://www.youtube.com/embed/pFRPEHwxseY",
+                "content": "blablablablablablablablablablabla blablablablablablablablablablabl ablablablablablablablablabla",
+                "title": "Recordar",
+                "share": "",
+                "like": ""
+            }]
+
+        },
+        "footer": {
+            "content": "Create, Design and Copy right reserved by",
+            "company": "La fabrica de bots © 2018"
+        }
+    }
+};
---
+++ home/home.js
@@ -0,0 +1,372 @@
+var text = function () {
+    return HomeData[Locale];
+};
+
+var imageUrl = ImageSource + "/home/image/";
+
+function ShowCase(props) {
+    return React.createElement(
+        "section",
+        { id: "showcase", className: "showcase background-blue text-white" },
+        React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "div",
+                { className: "video" },
+                React.createElement("iframe", { src: "https://www.youtube.com/embed/V0FtjeFV1tg", frameBorder: "0" })
+            )
+        ),
+        React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "div",
+                { className: "text" },
+                React.createElement(
+                    "h1",
+                    null,
+                    text().showcase.title
+                ),
+                React.createElement(
+                    "div",
+                    null,
+                    React.createElement(
+                        "p",
+                        { className: "animateOne animated zoomIn" },
+                        text().showcase.content
+                    ),
+                    React.createElement(
+                        Button,
+                        { size: "small", href: "#chat", className: "text-white" },
+                        text().showcase.button
+                    )
+                )
+            )
+        )
+    );
+}
+
+function AboutCard(props) {
+    var data = props.data;
+    return React.createElement(
+        "div",
+        { className: "item background-green about-card" },
+        React.createElement(
+            Card,
+            null,
+            React.createElement(CardMedia, {
+                className: "itemMedia",
+                image: imageUrl + data.image,
+                title: data.title
+            }),
+            React.createElement(
+                CardContent,
+                null,
+                React.createElement(
+                    Typography,
+                    { gutterBottom: true, variant: "headline", component: "h2" },
+                    data.title
+                ),
+                React.createElement(
+                    Typography,
+                    { component: "p" },
+                    data.content
+                )
+            ),
+            React.createElement(
+                CardActions,
+                null,
+                React.createElement(
+                    Button,
+                    { size: "small", color: "primary" },
+                    React.createElement(ShareIcon, null)
+                ),
+                React.createElement(
+                    Button,
+                    { size: "small", color: "primary" },
+                    React.createElement(LikeIcon, null)
+                )
+            )
+        )
+    );
+}
+
+function About(props) {
+
+    var cards = text().about.data.map((data, index) => React.createElement(AboutCard, { key: index,
+        data: data
+    }));
+
+    return React.createElement(
+        "section",
+        { id: "about", className: "" },
+        React.createElement(
+            "h1",
+            null,
+            text().about.title
+        ),
+        React.createElement(
+            "div",
+            { className: "itemList" },
+            cards
+        )
+    );
+}
+
+function CreatorCard(props) {
+
+    var data = props.data;
+    return React.createElement(
+        "div",
+        { className: "smallItem creator-card" },
+        React.createElement(
+            Card,
+            null,
+            React.createElement(CardHeader, {
+                avatar: React.createElement(
+                    Avatar,
+                    { "aria-label": "Recipe" },
+                    data.avatar
+                ),
+                title: data.title,
+                subheader: data.profession
+            }),
+            React.createElement(
+                CardContent,
+                null,
+                React.createElement(
+                    Typography,
+                    { gutterBottom: true, variant: "headline", component: "h2" },
+                    data.name
+                ),
+                React.createElement(
+                    Typography,
+                    { component: "p" },
+                    data.content
+                )
+            ),
+            React.createElement(
+                CardActions,
+                null,
+                React.createElement(
+                    IconButton,
+                    { href: data.linkedin },
+                    React.createElement(LinkedinIcon, null)
+                ),
+                React.createElement(
+                    IconButton,
+                    { href: data.email },
+                    React.createElement(ContactMailIcon, null)
+                )
+            )
+        )
+    );
+}
+
+function Creator(props) {
+    var cards = text().creator.data.map((data, index) => React.createElement(CreatorCard, { key: index,
+        data: data
+    }));
+
+    return React.createElement(
+        "section",
+        { id: "creator", className: "background-green text-white" },
+        React.createElement(
+            "h1",
+            null,
+            text().creator.title
+        ),
+        React.createElement(
+            "div",
+            { className: "itemList" },
+            cards
+        )
+    );
+}
+
+function FunctionCard(props) {
+    var data = props.data;
+    return React.createElement(
+        "div",
+        { className: "item background-blue function-card" },
+        React.createElement(
+            Card,
+            null,
+            React.createElement(
+                CardMedia,
+                { src: "" },
+                React.createElement(
+                    "div",
+                    { className: "card-video" },
+                    React.createElement("iframe", { src: data.video, frameBorder: "0" })
+                )
+            ),
+            React.createElement(
+                CardContent,
+                null,
+                React.createElement(
+                    Typography,
+                    { gutterBottom: true, variant: "headline", component: "h2" },
+                    data.title
+                ),
+                React.createElement(
+                    Typography,
+                    { component: "p" },
+                    data.content
+                )
+            ),
+            React.createElement(
+                CardActions,
+                null,
+                React.createElement(
+                    Button,
+                    { size: "small", color: "primary" },
+                    React.createElement(ShareIcon, null)
+                ),
+                React.createElement(
+                    Button,
+                    { size: "small", color: "primary" },
+                    React.createElement(LikeIcon, null)
+                )
+            )
+        )
+    );
+}
+
+function Function(props) {
+
+    var cards = text().function.data.map((data, index) => React.createElement(FunctionCard, { key: index,
+        data: data
+    }));
+
+    return React.createElement(
+        "section",
+        { id: "function", className: "" },
+        React.createElement(
+            "h1",
+            null,
+            text().function.title
+        ),
+        React.createElement(
+            "div",
+            { className: "itemList" },
+            cards
+        )
+    );
+}
+
+class Chat extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            iframeUrl: "https://webchat.botframework.com/embed/d2cfab16?s=JzXx7BDwpbU.cwA.vho.ZssTGSuetrvi70O4HR7pHt3SYz2Avo1pXjgCNTjywts",
+            name: "Stico"
+        };
+    }
+
+    changeIframe(url, name) {
+
+        this.setState({
+            iframeUrl: this.state.iframeUrl,
+            url: url,
+            name: name
+        });
+    }
+
+
+    render() {
+
+        var content = React.createElement(
+            "div",
+            { id: "chatContent" },
+            React.createElement("iframe", { id: "chat-iframe", src: this.state.iframeUrl })
+        );
+
+        if (this.state.name !== 'Stico') {
+            content = React.createElement(
+                "div",
+                { id: "chatContent" },
+                React.createElement(
+                    Button,
+                    { target: "_blank", href: this.state.url, className: "chat-connect-button", size: "big", color: "primary" },
+                    React.createElement(SendIcon, null)
+                )
+            );
+        }
+
+        return React.createElement(
+            "section",
+            { id: "chat", className: "background-blue text-white" },
+            React.createElement("h1", null),
+            React.createElement(Channels, { onclick: (url, name) => {
+                    this.changeIframe(url, name);
+                }, className: "chat-channels background-blue text-white" }),
+            React.createElement("h1", null),
+            React.createElement(
+                "div",
+                { className: "chat" },
+                React.createElement(
+                    "h2",
+                    { id: "chatTitle", className: "chat-title background-blue" },
+                    this.state.name
+                ),
+                content
+            )
+        );
+    }
+}
+
+function Footer(props) {
+    var data = text().footer;
+    return React.createElement(
+        "section",
+        { id: "footer", className: "footer background-green text-white" },
+        React.createElement(
+            "p",
+            null,
+            data.content
+        ),
+        React.createElement(
+            "p",
+            null,
+            data.company
+        )
+    );
+}
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            locale: Locale
+        };
+        this.rightTitle = props.rightTitle;
+    }
+
+    render() {
+
+        this.rightTitle(text().rightTitle);
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(ShowCase, null),
+            React.createElement(About, null),
+            React.createElement(Creator, null),
+            React.createElement(Function, null),
+            React.createElement(Chat, null),
+            React.createElement(Footer, null)
+        );
+    }
+}
+
+window.onload = function () {
+
+    ReactDOM.render(React.createElement(
+        Page,
+        { title: text().title, rightTitle: text().rightTitle },
+        React.createElement(Home, null)
+    ), document.getElementById("page"));
+};
